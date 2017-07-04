d5bc9713341e5fa8665d515563980adf0413afc5
David Rubinstein <drubinstein91@gmail.com>
2017-07-04 19:18:39 -0400

    Adding baseline class to perform FFT
    This function is recursive but will eventually be used to submit tasks to spark. Each fft call should be a spark task. Also a class to handle complex numbers must be added.

--- src/main/scala/Fft.scala
+++ src/main/scala/Fft.scala
@@ -1,3 +1,6 @@
+package Fft
+
+import Complex.Complex
 /**
   * Created by david on 7/4/17.
   */
@@ -5,14 +8,16 @@ object Fft {
   def fft(signal: Vector[Complex], s: Int): Vector[Complex] = {
     if (signal.length == 1) signal
     else {
-      X0 = fft(signal.drop(2).grouped(2).map(_.head).toVector, 2 * s)
-      X1 = fft(signal.tail().drop(2).grouped(2).map(_.head).toVector, 2 * s)
+      val X0 = Fft.fft(signal.drop(2).grouped(2).map(_.head).toVector, 2 * s)
+      val X1 = Fft.fft(signal.tail.drop(2).grouped(2).map(_.head).toVector, 2 * s)
       for (k <- 0 until X0.length) {
-        t = X0(k)
-        X0(k) = t + Complex(-2 * math.Pi * s * k / signal.length) * X1(k)
-        X1(k) = t - Complex(-2 * math.Pi * s * k / signal.length) * X1(k)
+        val t = X0(k)
+        val omega = -2 * math.Pi * s * k / signal.length
+        val w = new Complex(Math.cos(omega), Math.sin(omega))
+        X0.updated(k, t + w * X1(k))
+        X1.updated(k, t + w * X1(k))
       }
-      X0 + X1
+      X0 ++ X1
     }
   }
 }
